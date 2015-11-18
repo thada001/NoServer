7c9d0c7c4731393b33fb325fda92eb46d051c910
Thomas Adair <thomascadair@gmail.com>
2015-11-18 16:38:12 -0600

    Added styling and functionality to table page fixtures list

--- noServer/js/tableCtrl.js
+++ noServer/js/tableCtrl.js
@@ -4,4 +4,14 @@ angular.module('eplApp').controller('tableCtrl', function($scope, tableService) 
       $scope.teams = res;
     });
   }();
+  $scope.getFixtures = function() {
+    tableService.getAllFixtures().then(function(res2) {
+      $scope.compFix = res2.comp;
+      $scope.upFix = res2.up;
+      console.log(res2);
+    })
+  }();
+  $scope.change = function() {
+    $scope.completed = !$scope.completed;
+  }
 })
--- noServer/js/tableService.js
+++ noServer/js/tableService.js
@@ -14,5 +14,27 @@ angular.module('eplApp').service('tableService', function($http, $q, fdata) {
       dfd.resolve(result);
     })
     return dfd.promise;
+  };
+  this.getAllFixtures = function() {
+    var dfd2 = $q.defer();
+    $http({
+      headers: {'X-Auth-Token': 'd69db8b392004af18d5b09fce1dba987'},
+      method: 'GET',
+      url: fdata.url + 'fixtures'
+    }).then(function(res2) {
+      var result = res2.data.fixtures;
+      var comp = [], up = [], results = {};
+      for (var i = 0; i < result.length; i++) {
+        if(result[i].status === 'FINISHED') {
+          comp.push(result[i]);
+        }else {
+          up.push(result[i]);
+        }
+      }
+      results.comp = comp;
+      results.up = up;
+      dfd2.resolve(results);
+    })
+    return dfd2.promise;
   }
 })
